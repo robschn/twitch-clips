5937aad7125105f50e9a946e13f0651dde93f5dd
robert.schneider <robert.schneider@stackpath.com>
2022-03-04 15:46:01 -0500

    Adds auth and getClips funcs

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/kelr/gundyr"
+)
+
 func main() {
-	auth()
+
+	// authenticate and grab session
+	session := auth(os.Getenv("TWITCH_ID"), os.Getenv("TWITCH_SECRET"))
+
+	// grab video clips URL
+	vidURLs := getVidClips(session, "ohgustie")
+
+	fmt.Printf("vidURLs: %v\n", vidURLs)
 }
 
-// curl https://clips-media-assets2.twitch.tv/AT-cm%7C1285425153.mp4 -o thumb.mp4
+func auth(clientID string, clientSecret string) *gundyr.Helix {
+	// Authenticate
+	cfg := &gundyr.HelixConfig{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+	}
+
+	c, err := gundyr.NewHelix(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return c
+}
+
+func getVidClips(c *gundyr.Helix, username string) []string {
+
+	// convert username to user ID
+	userID, err := c.UserToID(username)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clips, err := c.GetAllClips(userID, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// initialize video URL slice
+	vidURLs := []string{}
+
+	for _, v := range clips {
+
+		if strings.Contains(v.ThumbnailURL, "AT-cm%") {
+			splitURL := strings.Split(v.ThumbnailURL, "-preview-")
+
+			// base URL will be the first element, add .mp4
+			vidURLs = append(vidURLs, splitURL[0]+".mp4")
+		}
+	}
+	return vidURLs
+}
